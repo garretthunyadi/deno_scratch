4ac19650216bd3d4a96b2e8cfdf7e808dafc30f8
Garrett Hunyadi <garrett@happify.com>
2022-06-24 16:11:50 -0400

    try to access web service

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { get_lgvs_by_name } from "./compass_service/compass_service.ts";
+
+let lgvs = await get_lgvs_by_name("localhost:8080", ["_last_text_input"])
 
 serve((_req: any) => {
-  return new Response("Hello World4!", {
+  // let foo = await get_lgv_by_id("http://localhost:8080", 1)
+
+  return new Response(`Hello World w lgvs:! ${lgvs[0]}`, {
     headers: { "content-type": "text/plain" },
   });
 });
