bc652e69262f75309bda5a36e68c0f5d032b2a6d
Garrett Hunyadi <garrett@happify.com>
2022-06-24 16:13:06 -0400

    try to access web service

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { get_lgvs_by_name } from "./compass_service/compass_service.ts";
 
-let lgvs = await get_lgvs_by_name("localhost:8080", ["_last_text_input"])
 
-serve((_req: any) => {
+serve(async (_req: any) => {
+  let lgvs = await get_lgvs_by_name("localhost:8080", ["_last_text_input"])
   // let foo = await get_lgv_by_id("http://localhost:8080", 1)
 
   return new Response(`Hello World w lgvs:! ${lgvs[0]}`, {
