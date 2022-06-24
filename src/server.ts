8765930fff43f5d330a5245dbf0151c75663c6f7
Garrett Hunyadi <garrett@happify.com>
2022-06-24 16:16:06 -0400

    try to access web service

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { get_lgvs_by_name } from "./compass_service/compass_service.ts";
+import { get_lgv_list } from "./compass_service/compass_service.ts";
 
 
 serve(async (_req: any) => {
-  // let lgvs = await get_lgvs_by_name("localhost:8080", ["_last_text_input"])
+  let lgvs = await get_lgv_list('compass-gw-qa01.happify.com')
   // let foo = await get_lgv_by_id("http://localhost:8080", 1)
 
-  return new Response(`Hello World w lgvs:! NOT`, {
+  return new Response(`Hello World w lgvs:! ${lgvs[0]}`, {
     headers: { "content-type": "text/plain" },
   });
 });
