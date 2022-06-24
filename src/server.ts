0aeeb3e19e4878b479c36c7f194e41c323140a1d
Garrett Hunyadi <garrett@happify.com>
2022-06-24 16:14:12 -0400

    try to access web service

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ import { get_lgvs_by_name } from "./compass_service/compass_service.ts";
 
 
 serve(async (_req: any) => {
-  let lgvs = await get_lgvs_by_name("localhost:8080", ["_last_text_input"])
+  // let lgvs = await get_lgvs_by_name("localhost:8080", ["_last_text_input"])
   // let foo = await get_lgv_by_id("http://localhost:8080", 1)
 
-  return new Response(`Hello World w lgvs:! ${lgvs[0]}`, {
+  return new Response(`Hello World w lgvs:! NOT`, {
     headers: { "content-type": "text/plain" },
   });
 });
